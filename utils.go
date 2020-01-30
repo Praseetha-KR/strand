388a1c3f9361244ef31689c5a1dddc285e25ce7c
Praseetha-KR <praseetha04@gmail.com>
2020-01-30 14:39:56 +0530

    Added tests for utils

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package strand
 
 import "math/rand"
 
+// shuffles the provided string
 func shuffle(s string) string {
 	b := []byte(s)
 	rand.Shuffle(len(b), func(i, j int) {
@@ -10,10 +11,13 @@ func shuffle(s string) string {
 	return string(b)
 }
 
+// randInt returns random integer from range [min, max]
 func randInt(min, max int) int {
-	return rseed.Intn(max-min) + min
+	return rseed.Intn((max+1)-min) + min
 }
 
+// randSizedString returns string of random characters
+// with length range (min, max)
 func randSizedString(min int, max int, charset string) string {
 	r := randInt(min, max)
 	return random(r, charset)
---
+++ utils_test.go
@@ -0,0 +1,68 @@
+package strand
+
+import (
+	"sort"
+	"strings"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+// --- Test Helpers ---
+
+type ByRune []rune
+
+func (r ByRune) Len() int           { return len(r) }
+func (r ByRune) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r ByRune) Less(i, j int) bool { return r[i] < r[j] }
+
+func stringToRuneSlice(s string) []rune {
+	var r []rune
+	for _, c := range s {
+		r = append(r, c)
+	}
+	return r
+}
+
+func isAnagram(s1, s2 string) bool {
+	var r1 ByRune = stringToRuneSlice(s1)
+	var r2 ByRune = stringToRuneSlice(s2)
+
+	sort.Sort(r1)
+	sort.Sort(r2)
+
+	return string(r1) == string(r2)
+}
+
+// --- /Test Helpers ---
+
+func TestShuffle(t *testing.T) {
+	s := "Hello, World!"
+	s1 := shuffle(s)
+
+	assert.Assert(t, s != s1)
+	assert.Equal(t, len(s), len(s1))
+	assert.Assert(t, isAnagram(s, s1))
+}
+
+func TestRandInt(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		n := randInt(1, 3)
+		assert.Assert(t, n == 1 || n == 2 || n == 3)
+	}
+}
+
+func TestRandSizedString(t *testing.T) {
+	s := randSizedString(1, 13, "abc")
+	l := len(s)
+	assert.Assert(t, l >= 1 && l <= 13)
+
+	// should only contain charset
+	s1 := ""
+	for _, c := range s {
+		if !strings.Contains("abc", string(c)) {
+			s1 = s1 + string(c)
+		}
+		assert.Assert(t, s1 == "")
+	}
+}
